f065c23ade47f32e7fd365530ee685f6b278ec9c
Naku401 <nahuelcruzcc@gmail.com>
2025-04-04 19:47:33 -0300

    naku401.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/naku401.py
@@ -0,0 +1,16 @@
+# Https://python.org
+
+# Comentario en una linea
+
+¨¨¨¨
+comentario
+en 
+varias 
+lineas
+¨¨¨¨
+
+MY_VARIABLE = ¨primer variable¨
+MY_VARIABLE = ¨segunda variable¨
+
+MY_CONSTANT = ¨mi constante¨
+MY_CONSTANT = ¨constante¨
